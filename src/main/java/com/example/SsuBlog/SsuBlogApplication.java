1a1420008201d6297a58536655cc405893922eb6
jinsu291 <wlstn291@naver.com>
2022-12-08 00:22:42 +0900

    setting

---
+++ src/main/java/com/example/SsuBlog/SsuBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.SsuBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuBlogApplication.class, args);
+	}
+
+}
