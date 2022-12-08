11e5bc0127c1e34c712e945577ed262fbe022c0f
jinsu291 <wlstn291@naver.com>
2022-12-08 15:50:20 +0900

    make db-init.sql

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+# DEV DB
+DROP DATABASE IF EXISTS SsuBlog__dev;
+CREATE DATABASE SsuBlog__dev;
+USE SsuBlog__dev;
+
+# TEST DB
+DROP DATABASE IF EXISTS SsuBlog__test;
+CREATE DATABASE SsuBlog__test;
+USE SsuBlog__test;
