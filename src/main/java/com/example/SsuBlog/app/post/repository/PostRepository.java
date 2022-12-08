80376434c26ba95ffca58eae9c01a7d2a2836f36
jinsu291 <wlstn291@naver.com>
2022-12-09 02:09:10 +0900

    PostTests - testJpa4-6

--- src/main/java/com/example/SsuBlog/app/post/repository/PostRepository.java
+++ src/main/java/com/example/SsuBlog/app/post/repository/PostRepository.java
@@ -3,6 +3,12 @@ package com.example.SsuBlog.app.post.repository;
 import com.example.SsuBlog.app.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
     Post findBySubject(String subject);
+
+    Post findBySubjectAndContent(String subject, String content);
+
+    List<Post> findBySubjectLike(String s);
 }
--- src/test/java/com/example/SsuBlog/PostTests.java
+++ src/test/java/com/example/SsuBlog/PostTests.java
@@ -1,5 +1,6 @@
 package com.example.SsuBlog;
 
+import com.example.SsuBlog.app.comment.repository.CommentRepository;
 import com.example.SsuBlog.app.post.entity.Post;
 import com.example.SsuBlog.app.post.repository.PostRepository;
 import org.junit.jupiter.api.Test;
@@ -7,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class PostTests {
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Test
     void contextLoads() {
@@ -53,5 +58,40 @@ public class PostTests {
         // SELECT * FROM question
         Post p =  postRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, p.getId());
+    }
+
+    @Test
+    void testJpa4() {
+        Post p = this.postRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, p.getId());
+    }
+
+    @Test
+    void testJpa5() {
+        List<Post> pList = this.postRepository.findBySubjectLike("sbb%");
+        Post p = pList.get(0);
+        assertEquals("sbb가 무엇인가요?", p.getSubject());
+    }
+
+    @Test
+    void testJpa6() {
+        Optional<Post> op = this.postRepository.findById(1);
+        assertTrue(op.isPresent());
+        Post q = op.get();
+        q.setSubject("수정된 제목");
+        this.postRepository.save(q); // UPDATE
+    }
+
+    @Test
+    void testJpa7() {
+        assertEquals(2, this.postRepository.count());
+        Optional<Post> oq = this.postRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Post q = oq.get();
+
+        this.postRepository.delete(q);
+
+        assertEquals(1, this.postRepository.count());
     }
 }
